82dd7ccb2a7732f4d8e7edcfe3f2eba538338d1a
Ilias El aabbasi <ie-laabb@e1r11p10.1337.ma>
2022-08-30 22:30:56 +0100

    only last ex

--- cpp01/ex04/Replace.cpp
+++ cpp01/ex04/Replace.cpp
@@ -5,28 +5,41 @@
 void	File::ReplaceFuc(){
 	std::ifstream	MyFile(name);
 	std::ofstream	NewFile(name + ".replace");
-	std::string		str;
+	std::string		temp;
+	std::string		temp2;
 	std::string		s;
+	unsigned long	count = 0;
 	// unsigned long	i = 0;
 	if(!MyFile)
 	{
 		std::cout << "file not found :(" << std::endl;
 		return ;
 	}
-	while (getline(MyFile, str)){
-		s += str;
-	}
-	std::cout << s << std::endl;/*
-	while (i < s.length())
-	{
-		if (s.compare(i, s1.length(), s2) == 0)
+	while (getline(MyFile, s)){
+		count = s.find(s1, 0);
+		if (count == std::string::npos)
 		{
-			s.erase(i, s1.length());
-			s.insert(i, s2);
+			temp2 = s;
 		}
-		i++;
+		temp = s;
+		while (count != std::string::npos){
+			temp2 = temp.substr(0, count);
+			temp2 += s2;
+			temp2 += &temp[count+s1.length()];
+			temp = temp2;
+			count = temp.find(s1, 0);
+		}
+		// std::cout << '|' << temp  << '|' << std::endl;
+		NewFile << temp2;
+		if (!MyFile.eof())
+			NewFile << std::endl;
+		temp2 = "";
 	}
-	NewFile << s;*/
 	MyFile.close();
 }
 
+/*
+found=str.find("haystack");
+  if (found!=std::string::npos)
+    std::cout << "'haystack' also found at: " << found << '\n';
+*/
