1fc678028d19a70d55909ab4732be8952f25f1e0
Ilias El aabbasi <ie-laabb@e2r8p8.1337.ma>
2022-10-24 23:11:33 +0100

    finito

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -3,21 +3,78 @@
 #include "C.hpp"
 
 Base* generate( void ){
-    Base a;
     srand(time(NULL));
-    int r = rand();
-    if (r % 3 == 0)
-        return (reinterpret_cast<A*>(&a));
-    else if (r % 3 == 1)
-        return (reinterpret_cast<B*>(&a));
-    return (reinterpret_cast<C*>(&a));
+
+    if (rand() % 3 == 0)
+       return (new A());
+    else if (rand() % 3 == 1)
+        return (new B());
+    return (new C());
 };
 
-void    identify(base* p) {
-    
+void    identify(Base* p) {
+    A*  a;
+    B*  b;
+    C*  c;
+
+    a = dynamic_cast<A*>(p);
+    b = dynamic_cast<B*>(p);
+    c = dynamic_cast<C*>(p);
+    if (a)
+        std::cout << "The actual type of this object is A" << std::endl;
+    if (b)
+        std::cout << "The actual type of this object is B" << std::endl;
+    if (c)
+        std::cout << "The actual type of this object is C" << std::endl;
 };
+
+void    identify(Base& p)
+{
+    try{
+        A& a = dynamic_cast<A&>(p);
+        std::cout << "The actual type of this object is A" << std::endl;
+        (void)a;
+        return ;
+    }
+    catch (std::exception &e){
+        (void)e;
+    }
+// ********************************************************
+    try{
+        B& b = dynamic_cast<B&>(p);
+        std::cout << "The actual type of this object is B" << std::endl;
+        (void)b;
+        return ;
+    }
+    catch (std::exception &e){
+        (void)e;
+    }
+// ********************************************************
+    try{
+        C& c = dynamic_cast<C&>(p);
+        std::cout << "The actual type of this object is C" << std::endl;
+        (void)c;
+        return ;
+    }
+    catch (std::exception &e){
+        (void)e;
+    }
+}
 
 
 int main() {
-    
+    A    _A;
+    B    _B;
+    C    _C;
+
+    identify(&_A);
+    identify(&_B);
+    identify(&_C);
+
+    std::cout << std::endl;
+    identify(generate());
+    std::cout << std::endl;
+    identify(_A);
+    identify(_B);
+    identify(_C);
 }
