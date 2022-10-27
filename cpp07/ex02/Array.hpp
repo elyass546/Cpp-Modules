4aab6a8c249cbb0f88555e8a8b47768ca40132d5
Ilias El aabbasi <ie-laabb@e2r5p10.1337.ma>
2022-10-27 21:03:00 +0100

    27-09-22

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,15 +1,37 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
-
+#include <iostream>
+#include <string>
+template<typename T>
 class Array {
     private:
-        /* data */
+        unsigned int    _N;
+        T*              _Array;
     public:
-        Array( void );
-        Array( unsigned int );
-        Array( Array const& );
-        Array& operator=( Array const& );
-        ~Array();
+        Array( void ){
+            _N = 0;
+            _Array = new T();
+            _Array = nullptr;
+        }
+        Array( unsigned int n) : _N(n), _Array(new T[n]()){}
+        Array( Array const& a) : _N(a._N), _Array(new T[n]()) {
+            for (int i = 0 ; i < a._N; i++){
+                _Array[i] = a._Array[i];
+            }
+        }
+        Array*   operator new[]( Array const & A){
+            Array*  NewArray = new T[A._N]();
+            return (NewArray);
+        }
+        Array& operator=( Array const& a) : _N(a._N), _Array(new T[a._N]()) {
+            for (int i = 0 ; i < a._N; i++){
+                _Array[i] = a._Array[i];
+            }
+            return (*this);
+        }
+        unsigned int getN( void ){return _N;}
+        T*  getArray( void ) {return _Array;}
+        ~Array(){}
 };
 
 
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,7 @@
+#include "Array.hpp"
+
+
+int main(){
+    Array<std::string>  Array(6);
+    // Array<std::string>    stringArray(10);
+}
